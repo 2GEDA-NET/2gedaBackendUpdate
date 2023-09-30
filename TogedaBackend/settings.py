e9379f111194417ecc515f4a28fea4a9a240a61a
2gedaFullStack <2gedafullstack@gmail.com>
2023-09-30 22:57:52 +0100

    generated static file

--- TogedaBackend/settings.py
+++ TogedaBackend/settings.py
@@ -191,10 +191,13 @@ AUTH_USER_MODEL = 'user.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Default primary key field type

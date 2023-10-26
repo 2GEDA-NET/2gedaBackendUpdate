36cc0f17f061182ec3e14f6aac41f1f71afc598b
2gedaFullStack <2gedafullstack@gmail.com>
2023-10-26 10:37:19 +0100

    asgi.py modified

--- TogedaBackend/asgi.py
+++ TogedaBackend/asgi.py
@@ -1,19 +1,15 @@
-"""
-ASGI config for myproject project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
 import chat.routing
+import logging
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TogedaBackend.settings')
+
+# Configure logging
+logger = logging.getLogger(__name__)
+logger.debug(f'DJANGO_SETTINGS_MODULE is set to: {os.environ.get("DJANGO_SETTINGS_MODULE")}')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),

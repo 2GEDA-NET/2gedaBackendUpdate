1228a6bc520cbd989c7ff03d9a196577993169ec
2gedaFullStack <2gedafullstack@gmail.com>
2023-10-28 13:14:28 +0100

    Updated CORS Header

--- TogedaBackend/settings.py
+++ TogedaBackend/settings.py
@@ -33,7 +33,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
-
+CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [

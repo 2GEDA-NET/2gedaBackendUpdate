294057c160dd801344117831357e490351277316
2gedaFullStack <2gedafullstack@gmail.com>
2023-10-31 13:59:15 +0100

    changed profile status view

--- user/views.py
+++ user/views.py
@@ -288,43 +288,15 @@ def update_user_profile(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# class UserProfileUpdateStatusView(APIView):
-#     authentication_classes = [TokenAuthentication,]
-#     # permission_classes = [IsAuthenticated]
+class UserProfileHasUpdatedProfileView(generics.RetrieveAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
 
-#     def get(self, request):
-#         # Get the current user based on the request
-#         user = request.user
-
-#         if user.is_authenticated:
-#             # Retrieve the user's profile
-#             try:
-#                 user_profile = UserProfile.objects.get(user=user)
-#                 has_updated_profile = user_profile.has_updated_profile
-#                 return Response({'has_updated_profile': has_updated_profile})
-#             except UserProfile.DoesNotExist:
-#                 return Response({'error': 'User profile not found'}, status=status.HTTP_NOT_FOUND)
-#         else:
-#             return Response({'error': 'User not authenticated'}, status=status.HTTP_UNAUTHORIZED)
-
-
-@api_view(['GET'])
-@authentication_classes([TokenAuthentication])
-# @permission_classes([IsAuthenticated])
-def user_profile_update_status(request):
-    # Get the current user based on the request
-    user = request.user
-
-    if user.is_authenticated:
-        # Retrieve the user's profile
-        try:
-            user_profile = UserProfile.objects.get(user=user)
-            has_updated_profile = user_profile.has_updated_profile
-            return Response({'has_updated_profile': has_updated_profile})
-        except UserProfile.DoesNotExist:
-            return Response({'error': 'User profile not found'}, status=status.HTTP_NOT_FOUND)
-    else:
-        return Response({'error': 'User not authenticated'}, status=status.HTTP_UNAUTHORIZED)
+    def get_object(self):
+        # Retrieve the user's profile based on the authenticated user
+        user = self.request.user
+        profile, created = UserProfile.objects.get_or_create(user=user)
+        return profile
 
 
 @api_view(['POST'])

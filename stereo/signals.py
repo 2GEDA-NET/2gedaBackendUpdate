9fd5aac85decd6345acbc811222bf84b77a3eb85
2gedaFullStack <2gedafullstack@gmail.com>
2023-11-16 15:47:11 +0100

    added Stereo account and Artist to admin

--- stereo/signals.py
+++ stereo/signals.py
@@ -1,18 +1,21 @@
-from django.db.models.signals import pre_save, post_save
-from django.dispatch import receiver
-from django.utils import timezone
-from django.core.exceptions import ValidationError
-from storages.backends.s3boto3 import S3Boto3Storage
 from pydub import AudioSegment
+from pydub.playback import play
+from django.core.exceptions import ValidationError
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from .models import Song
 from datetime import timedelta
 
 @receiver(post_save, sender=Song)
 def set_song_duration(sender, instance, **kwargs):
     try:
-        # Open the file using Django storage's open method
-        with instance.audio_file.open('rb') as audio_file:
-            audio = AudioSegment.from_file(audio_file)
+        # Load audio using pydub's AudioSegment.from_file method
+        audio = AudioSegment.from_file(instance.audio_file.path)
+
+        # Play the audio (this is optional and can be removed if not needed)
+        play(audio)
+
+        # Calculate duration
         duration_seconds = audio.duration_seconds
         instance.duration = timedelta(seconds=duration_seconds)
     except Exception as e:
